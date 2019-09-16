4aecabaf65bde17889e62cd10e165deac0dae619
Colden Miller <generalpineapple@ksu.edu>
2019-09-16 14:26:12 -0500

    Added cross product and magnitude

--- src/Vector3.cs
+++ src/Vector3.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace VectorMath
 {
     /// <summary>
@@ -37,15 +36,40 @@ namespace VectorMath
         /// <summary>
         /// Constructs a new instance of Vector3 with
         /// the supplied values for X, Y, and Z.
+        /// </summary>
         /// <param name="X">A double precision number for the x component of the vector.</param>
         /// <param name="Y">A double precision number for the y component of the vector.</param>
         /// <param name="Z">A double precision number for the z component of the vector.</param>
-        /// </summary>
         public Vector3(double x, double y, double z)
         {
             this.X = x;
             this.Y = y;
             this.Z = z;
+        }
+
+        /// <summary>
+        /// Gets the magnitude of this vector
+        /// </summary>
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.Y * this.Z);
+            }
+        }
+
+        /// <summary>
+        /// Calculates the cross product of theis and the other vector (maybe?)
+        /// </summary>
+        /// <param name="other">the vector to cross with this vector</param>
+        /// <returns>the cross product</returns>
+        public Vector3 CrossProduct(Vector3 other)
+        {
+            return new Vector3(
+                this.Y * other.Z - this.Z * other.Y,
+                this.Z * other.X - this.Z * other.Y,
+                this.X * other.Y - this.Y * other.X
+            );
         }
     }
 }
